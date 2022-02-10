9baa29c504f11663da7c6f01b29f8aac558b23c2
aintxg2g <98034229+aintxg2g@users.noreply.github.com>
2022-02-10 21:54:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import Accordian from './components/Accordian';
+
+function App() {
+  return (
+   
+     <Accordian />
+    
+  );
+}
+
+export default App;
